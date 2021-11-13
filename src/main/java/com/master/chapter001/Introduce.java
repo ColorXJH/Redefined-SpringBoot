23d6754b0f7cc4b2ab30d1e877841f103022fddc
ColorXJH <1204332316@qq.com>
2021-11-13 14:19:20 +0800

    redefined-springboot-first-commit

---
+++ src/main/java/com/master/chapter001/Introduce.java
@@ -0,0 +1,13 @@
+package com.master.chapter001;
+
+/**
+ * @Description:
+ * @Author: ColorXJH
+ * @CreateDate: 2021/11/13 14:18
+ * @Version: 1.0
+ */
+public class Introduce {
+    public static void main(String[] args) {
+
+    }
+}
