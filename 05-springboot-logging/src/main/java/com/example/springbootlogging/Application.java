86893dc8b84d40b1b3a5f80ef6c336c086bec09c
ColorXJH <1204332316@qq.com>
2021-11-22 23:06:07 +0800

    redefined-springboot-logging-commit-25

--- 05-springboot-logging/src/main/java/com/example/springbootlogging/Application.java
+++ 05-springboot-logging/src/main/java/com/example/springbootlogging/Application.java
@@ -18,3 +18,43 @@ public class Application {
     //JUL JCL Jboss-logging logback log4j log4j2 slf4j
     //日志门面：JCL  SLF4J JBoss-logging    //日志实现 log4j  JUL  log4j2  logback    门面+实现==》SLF4J+logback
 //springboot:底层是spring框架，spring框架默认使用JCL(jakarta commons logging),springboot选用slf4j+logback
+    //如何再系统中使用slf4j:在开发的时候，日志记录，调用日志记录方法，不应该直接调用实现类，而是调用抽象层的方法
+        //1:系统导入slf4j的jar包和logback的实现jar ==>http://www.slf4j.org/manual.html
+//每一个日志的实现框架都有自己的配置文件，使用slf4j之后，配置文件还是做成日志实现框架的配置文件;
+    //遗留问题：a(slf4j+logback)：Spring(commons-logging)  Hibernate(jboss-logging)  Mybatis XXX...
+    //统一日志记录，即使是别的框架和我们一起，统一使用slf4j进行输出
+        //1：先将系统中其他日志框架排除，
+        //2：用中间包来替换原有的日志框架
+        //3：我们再来导入slf4j其他的实现
+//springboot 依赖一下组件实现日志功能
+/*
+<dependency>
+<groupId>org.springframework.boot</groupId>
+<artifactId>spring-boot-starter-logging</artifactId>
+<version>2.6.0</version>
+<scope>compile</scope>
+</dependency>
+*/
+//springboot底层也是使用slf4j+logback方式来记录日志
+//springboot也把其他的日志都替换成的slf4j==>jul-to-slf4j,log4j-over-slf4j
+//如果我们要引入其他框架，一定要把这个框架的默认日志依赖移除掉
+    //spring框架使用的是commons-logging，所以springboot引入spring的时候派出了它内部的commons-logging依赖，springboot能适配所有的日志，底层使用slf4j+logback，唯一需要注意的是
+    //在引入其他框架的时候需要将这个框架依赖的日志排除掉
+/*
+<dependency>
+<groupId>org.apache.activemq</groupId>
+<artifactId>activemq-console</artifactId>
+<version>${activemq.version}</version>
+<exclusions>
+<exclusion>
+<groupId>commons-logging</groupId>
+<artifactId>commons-logging</artifactId>
+</exclusion>
+</exclusions>
+</dependency>
+*/
+
+//默认配置:springboot默认帮我们配置好了日志
+//指定配置：给类路径下放上每个日志框架自己的配置文件即可，springboot就不使用默认配置了，
+    //关于springboot的配置规则文件命名，可以查看springboot官方文档，有一节详细介绍了log配置文件
+    //logback.xml直接被日志框架识别 logback-spring.xml被springboot加载识别，可以使用springboot的部分高级功能，可以指定某段配置只在某个环境下生效,springProfile
--- 05-springboot-logging/src/test/java/com/example/springbootlogging/ApplicationTests.java
+++ 05-springboot-logging/src/test/java/com/example/springbootlogging/ApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.springbootlogging;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +10,14 @@ class ApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("---开始记录日志---");
+        Logger logger = LoggerFactory.getLogger(getClass());//alt+enter:自动提示补全
+        //日志的级别：由低到高，可以调整需要输出的日志级别，日志就会在这个日志及其高的级别生效，默认使用info级别（及以上）日志,可以在配置文件中给某个包，类配置指定的日志级别
+        logger.trace("这是trace日志");
+        logger.debug("这是debug日志");
+        logger.info("这是info日志");
+        logger.warn("这是warn日志");
+        logger.error("这是error日志");
     }
 
 }
