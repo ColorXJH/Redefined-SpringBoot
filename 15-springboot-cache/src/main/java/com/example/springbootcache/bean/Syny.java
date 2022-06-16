6d7a95c63aad59d76d3ab23ca2a6467e867bfb40
ColorXJH <1204332316@qq.com>
2022-06-16 19:36:07 +0800

    redefined-springboot-cache-74

---
+++ 15-springboot-cache/src/main/java/com/example/springbootcache/bean/Syny.java
@@ -0,0 +1,14 @@
+package com.example.springbootcache.bean;
+
+/**
+ * @ClassName: Syny
+ * @Package: com.example.springbootcache.bean
+ * @Description:
+ * @Datetime: 2022/6/16 18:46
+ * @author: ColorXJH
+ */
+public class Syny {
+    public static void main(String[] args) {
+        System.out.println("小金同学你很棒棒哟~");
+    }
+}
