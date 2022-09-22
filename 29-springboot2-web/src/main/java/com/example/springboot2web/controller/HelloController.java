a03bed45bd021153025268e042dd7f874400db0b
Master <1204332316@qq.com>
2022-09-22 16:56:08 +0800

    redefined-springboot-141 springboot2 静态资源访问规则以及配置 常用参数注解使用以及 @RequestAttribute

--- 29-springboot2-web/src/main/java/com/example/springboot2web/controller/HelloController.java
+++ 29-springboot2-web/src/main/java/com/example/springboot2web/controller/HelloController.java
@@ -2,6 +2,11 @@ package com.example.springboot2web.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author ColorXJH
  * @version 1.0
@@ -32,5 +37,41 @@ public class HelloController {
     public String putUser(){
         return "put-user";
     }
+
+    @GetMapping("/test/{id}/{name}")
+    public Object test(@PathVariable("id") String id,
+                       @PathVariable("name") String name,
+                       @PathVariable Map<String,String> values,
+                       @RequestHeader("User-Agent")String agent,
+                       @RequestHeader Map<String,String> heades,
+                       @RequestParam("car")String car,
+                       @RequestParam("price")Integer price,
+                       @RequestParam("interest") List<String> interest,
+                       @RequestParam Map<String,String> params,
+                       @CookieValue("Idea-3defc7d9")String cookieValue,
+                       @CookieValue("Idea-3defc7d9")Cookie cookie){
+        Map<String,Object> map=new HashMap<>();
+        map.put("id",id);
+        map.put("name",name);
+        map.put("values",values);
+        map.put("user-agent",agent);
+        map.put("headers",heades);
+        map.put("car",car);
+        map.put("price",price);
+        map.put("interest",interest);
+        map.put("params",params);
+        map.put("cookieValue",cookieValue);
+        map.put("cookie",cookie);
+
+        return map;
+    }
+    /*获取表单请求体信息*/
+    @PostMapping("/test2")
+    public Map test2(@RequestBody String content){
+        Map<String ,Object>map=new HashMap<>();
+        map.put("content",content);
+        return map;
+    }
+
 
 }
--- 29-springboot2-web/src/main/java/com/example/springboot2web/controller/ReIndexController.java
+++ 29-springboot2-web/src/main/java/com/example/springboot2web/controller/ReIndexController.java
@@ -2,6 +2,12 @@ package com.example.springboot2web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author ColorXJH
@@ -19,5 +25,26 @@ public class ReIndexController {
     @GetMapping("/")
     public String getIndex(){
         return "login";//默认static目录下的login.html
+    }
+
+
+    @GetMapping("/goto")
+    public String gotoPage(HttpServletRequest request){
+        request.setAttribute("msg","hello-world");
+        request.setAttribute("code","java");
+        return "forward:/success";//转发到/success请求
+    }
+
+    @GetMapping("/success")
+    @ResponseBody
+    public Map success(@RequestAttribute("msg") String msg,
+                          @RequestAttribute("code") String code,
+                          HttpServletRequest request){
+        Object o=request.getAttribute("msg");
+        Map<String,Object> map=new HashMap<>();
+        map.put("msg",msg);
+        map.put("code",code);
+        map.put("o",o);
+        return map;
     }
 }
