781b11b88eaf1ed0d1b78c005f2ee49b825bf395
Master <1204332316@qq.com>
2022-10-17 11:09:38 +0800

    redefined-springboot-191 spring-boot-admin-starter-server client与server

--- 30-springboot2-thymeleaf/src/main/java/com/example/springboot2thymeleaf/controller/TableController.java
+++ 30-springboot2-thymeleaf/src/main/java/com/example/springboot2thymeleaf/controller/TableController.java
@@ -5,11 +5,8 @@ import com.example.springboot2thymeleaf.bean.MPUser;
 import com.example.springboot2thymeleaf.mapper.UserMapper;
 import com.example.springboot2thymeleaf.service.TableService;
 import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.binder.MeterBinder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,7 +18,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.Resource;
 import java.util.List;
-import java.util.Queue;
 
 /**
  * @author ColorXJH
@@ -96,8 +92,8 @@ public class TableController {
         counter = registry.counter("TableController.count");
     }
     //除了在构造器中传入MeterRegistry，也可以使用下面的写法(在有@Configuration注解类中书写)
-    @Bean
+    /*@Bean
     MeterBinder queueSize(Queue queue){
         return (registry)-> Gauge.builder("queueSize",queue::size).register(registry);
-    }
+    }*/
 }
--- 31-springboot2-adminserver/src/main/java/com/example/springboot2adminserver/Application.java
+++ 31-springboot2-adminserver/src/main/java/com/example/springboot2adminserver/Application.java
@@ -1,8 +1,9 @@
 package com.example.springboot2adminserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableAdminServer
 @SpringBootApplication
 public class Application {
 
