c2e8c91b10777377a614b395c5f86c36312bc22e
Master <1204332316@qq.com>
2022-10-19 17:07:24 +0800

    redefined-springboot-196 源码分析 springboot完整启动过程

--- 34-springboot2-use-starter/src/main/java/com/example/springboot2usestarter/Application.java
+++ 34-springboot2-use-starter/src/main/java/com/example/springboot2usestarter/Application.java
@@ -10,4 +10,13 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
     //SpringApplication启动器的创建与运行过程
+
+    //@PropertySource配置的用法
+    //加载指定的属性文件（*.properties）到 Spring 的 Environment 中。可以配合 @Value 和
+    //@ConfigurationProperties 使用。
+    //@PropertySource 和 @Value
+    //组合使用，可以将自定义属性文件中的属性变量值注入到当前类的使用@Value注解的成员变量中。
+    //@PropertySource 和 @ConfigurationProperties
+    //组合使用，可以将属性文件与一个Java类绑定，将属性文件中的变量值注入到该Java类的成员变量中。
+
 }
